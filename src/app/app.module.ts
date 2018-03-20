213bd4858306a6bb5eecdaaf36e056b88c1ff9e5
Budi Irawan <budi.irawan@invoice2go.com>
2018-03-21 01:43:04 +1100

    phone list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PhoneListComponent } from './phone-list/phone-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/phone-list/phone-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Phone } from '../phone.model';
+
+@Component({
+  selector: 'app-phone-list',
+  templateUrl: './phone-list.component.html',
+  styleUrls: ['./phone-list.component.css']
+})
+export class PhoneListComponent implements OnInit {
+  phones: Phone[] = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
